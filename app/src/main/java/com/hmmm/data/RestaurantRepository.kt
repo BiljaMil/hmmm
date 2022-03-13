9f3b488df0dfe45f4892aec73d081237f73ae0f0
Daniel Schruhl <danielschruhl@gmail.com>
2022-03-13 16:45:16 +0100

    Implement random restaurant recommendation

--- app/src/main/java/com/hmmm/data/RestaurantRepository.kt
+++ app/src/main/java/com/hmmm/data/RestaurantRepository.kt
@@ -1,4 +1,14 @@
 package com.hmmm.data
 
+import com.hmmm.domain.Restaurant
+
 class RestaurantRepository {
+    private val restaurants = listOf(
+        Restaurant("Chutnify"),
+        Restaurant("Billys bruzzelhütte")
+    )
+
+    fun findRestaurants(): List<Restaurant> {
+        return this.restaurants
+    }
 }
--- app/src/main/java/com/hmmm/domain/RestaurantService.kt
+++ app/src/main/java/com/hmmm/domain/RestaurantService.kt
@@ -1,7 +1,14 @@
 package com.hmmm.domain
 
+import com.hmmm.data.RestaurantRepository
+import kotlin.random.Random
+
 class RestaurantService {
-    public fun recommend(): Restaurant {
-        throw NotImplementedError("Not implemented yet")
+    fun recommend(): Restaurant {
+        val restaurantRepository = RestaurantRepository()
+        val maxIndex = restaurantRepository.findRestaurants().size
+        val minIndex = 0
+        val randomIndex = Random.nextInt(minIndex, maxIndex)
+        return restaurantRepository.findRestaurants()[randomIndex]
     }
 }
